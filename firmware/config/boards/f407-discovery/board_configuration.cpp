1c321c1c45f7284aa2327049412f52e8be5a3c14
AGT9000 <84887759+AGT9000@users.noreply.github.com>
2024-09-26 21:39:33 +0700

    Chage Clt ADC

--- firmware/config/boards/f407-discovery/board_configuration.cpp
+++ firmware/config/boards/f407-discovery/board_configuration.cpp
@@ -113,7 +113,6 @@ void setBoardDefaultConfiguration() {
 	engineConfiguration->clt.adcChannel = EFI_ADC_6;
 	engineConfiguration->iat.adcChannel = EFI_ADC_7;
 	engineConfiguration->afr.hwChannel = EFI_ADC_14;
-	engineConfiguration->clt.adcChannel = EFI_ADC_6;
 
 	engineConfiguration->accelerometerSpiDevice = SPI_DEVICE_1;
 

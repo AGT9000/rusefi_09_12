e6a24735b1c9bd6b9853bb3755aff1819a7e7164
AGT9000 <84887759+AGT9000@users.noreply.github.com>
2024-10-01 15:51:24 +0700

    only:f407-discovery

--- firmware/config/boards/f407-discovery/board_configuration.cpp
+++ firmware/config/boards/f407-discovery/board_configuration.cpp
@@ -20,7 +20,40 @@ static void setEtb(){
 	//setTPS1Calibration(100,809);
 	//setToyota1NZFE();
 }
+static void discovery_custom(){
 
+	engineConfiguration->map.sensor.hwChannel = EFI_ADC_12;
+	
+	engineConfiguration->clt.adcChannel = EFI_ADC_13;
+	
+	engineConfiguration->iat.adcChannel = EFI_ADC_10;
+	engineConfiguration->afr.hwChannel = EFI_ADC_NONE;
+	engineConfiguration->vbattAdcChannel = EFI_ADC_15;
+
+	engineConfiguration->fanPin = Gpio::E3;
+	engineConfiguration->fuelPumpPin = Gpio::E6;
+
+
+	engineConfiguration->injectionPins[0] = Gpio::D12;
+	
+	engineConfiguration->injectionPins[1] = Gpio::D14;
+	engineConfiguration->injectionPins[2] = Gpio::D13;
+	engineConfiguration->injectionPins[3] = Gpio::D15;
+	engineConfiguration->injectionPins[4] = Gpio::E9;
+	engineConfiguration->injectionPins[5] = Gpio::E11;
+
+	engineConfiguration->ignitionPins[0] = Gpio::D7;
+	engineConfiguration->ignitionPins[1] = Gpio::B9;
+	engineConfiguration->ignitionPins[2] = Gpio::A8;
+	engineConfiguration->ignitionPins[3] = Gpio::D9;
+	engineConfiguration->ignitionPins[4] = Gpio::D10;
+	engineConfiguration->ignitionPins[5] = Gpio::E15;
+
+
+	engineConfiguration->accelerometerSpiDevice = SPI_NONE;
+
+
+}
 static void setDefaultFrankensoStepperIdleParameters() {
 	engineConfiguration->idle.stepperDirectionPin = Gpio::E10;
 	engineConfiguration->idle.stepperStepPin = Gpio::E12;
@@ -107,6 +140,8 @@ static void configureAccelerometerPins() {
 void setBoardDefaultConfiguration() {
 	setDefaultFrankensoStepperIdleParameters();
 	setCanFrankensoDefaults();
+	discovery_custom()
+	setEtb();
 
 #ifndef HW_NOT_COMMUNITY_FRANKENSO
   // f407-discovery community board not frankenso
@@ -123,38 +158,9 @@ void setBoardDefaultConfiguration() {
 	configureAccelerometerPins();
 #endif /* EFI_ONBOARD_MEMS */
 
-	setEtb();
 
-	engineConfiguration->map.sensor.hwChannel = EFI_ADC_12;
+
 	
-	engineConfiguration->clt.adcChannel = EFI_ADC_13;
-	//engineConfiguration->iat.adcChannel = EFI_ADC_7;
-	engineConfiguration->iat.adcChannel = EFI_ADC_10;
-	engineConfiguration->afr.hwChannel = EFI_ADC_NONE;
-	engineConfiguration->vbattAdcChannel = EFI_ADC_15;
-
-	engineConfiguration->fanPin = Gpio::E3;
-	engineConfiguration->fuelPumpPin = Gpio::E6;
-
-
-	engineConfiguration->injectionPins[0] = Gpio::D12;
-	
-	engineConfiguration->injectionPins[1] = Gpio::D14;
-	engineConfiguration->injectionPins[2] = Gpio::D13;
-	engineConfiguration->injectionPins[3] = Gpio::D15;
-	engineConfiguration->injectionPins[4] = Gpio::E9;
-	engineConfiguration->injectionPins[5] = Gpio::E11;
-
-	engineConfiguration->ignitionPins[0] = Gpio::D7;
-	engineConfiguration->ignitionPins[1] = Gpio::B9;
-	engineConfiguration->ignitionPins[2] = Gpio::A8;
-	engineConfiguration->ignitionPins[3] = Gpio::D9;
-	engineConfiguration->ignitionPins[4] = Gpio::D10;
-	engineConfiguration->ignitionPins[5] = Gpio::E15;
-
-
-	engineConfiguration->accelerometerSpiDevice = SPI_NONE;
-
 /*
 
 	engineConfiguration->map.sensor.hwChannel = EFI_ADC_4;
